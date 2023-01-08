7769ff64344cf14507bce9b2acb72db8b993378c
aqswa <69039161+aqswa@users.noreply.github.com>
2023-01-08 22:41:41 +0900

    chore: hide secret key

--- CheerCharm/settings.py
+++ CheerCharm/settings.py
@@ -23,7 +23,6 @@ env = environ.Env(
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
