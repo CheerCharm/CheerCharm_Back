589606b73365b65f5dee84fc15d5ba5b11754b89
이나경 <101031854+rinarina0429@users.noreply.github.com>
2023-01-13 00:31:37 +0900

    Update urls.py

--- CheerCharm/urls.py
+++ CheerCharm/urls.py
@@ -19,6 +19,5 @@ from django.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('charms/', include('charms.urls')),
 ]
