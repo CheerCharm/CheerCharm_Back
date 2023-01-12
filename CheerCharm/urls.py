a26ff754c666c195bccf52e3414c473de52096f3
이나경 <101031854+rinarina0429@users.noreply.github.com>
2023-01-13 00:30:22 +0900

    Update urls.py

--- CheerCharm/urls.py
+++ CheerCharm/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('charms/', include('charms.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('charms/', include('charms.urls')),
 ]
