7e448f985c2a60115d2ce78c3afda9c6bae791e0
aqswa <69039161+aqswa@users.noreply.github.com>
2023-01-22 03:18:15 +0900

    Deploy: add health_check

--- CheerCharm/settings/base.py
+++ CheerCharm/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'accounts',
     'cheers',
     'charms',
+    'health_check',
 ]
 
 MIDDLEWARE = [
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,8 @@
 from .views import *
 from django.urls import path
 
-app_name='accounts'
+app_name = 'accounts'
 
-urlpatterns=[
-    path('signup/', SignUpView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
-    path('list/', ListView.as_view()),
+urlpatterns = [
+
 ]
