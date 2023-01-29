c0b99d107780ad6b280b6ba53ee9108f15ba284e
aqswa <69039161+aqswa@users.noreply.github.com>
2023-01-29 23:18:52 +0900

    Fix: set rotate refresh tokens to false

--- CheerCharm/settings/base.py
+++ CheerCharm/settings/base.py
@@ -151,8 +151,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
